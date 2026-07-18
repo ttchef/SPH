10b5f836423db47002ef50ff9b06ef55331a8cb4
ttchef <ruro0.edit@gmail.com>
2026-07-19 00:30:38 +0200

    little adjustments

--- src/shaders/sph.glsl
+++ src/shaders/sph.glsl
@@ -2,11 +2,10 @@
 
 #define PI 3.1415926538
 
-#define SMOOTHING_RADIUS 65.0
+#define SMOOTHING_RADIUS 50.0
 #define VISCOSITY_COEFF 5000.0
 
 // NOTE: Get from simulating in a grid
-// #define TARGET_DENSITY 0.005576722    
 #define TARGET_DENSITY 0.0044446052    
 
 struct particle
